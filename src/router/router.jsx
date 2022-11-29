8f7aa9260a96b30768762272204877126f968060
musfiqurofficial <musfiqurofficial1999@gmail.com>
2022-11-30 01:57:30 +0600

    error page create

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -27,7 +27,7 @@ export const router = createBrowserRouter([
     {
         path: '/',
         element: <Main></Main>,
-        errorElement: <ErrorPage></ErrorPage>
+        errorElement: <ErrorPage></ErrorPage>,
         children: [
             {
                 path: '/',
