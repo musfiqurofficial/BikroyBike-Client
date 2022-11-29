dc10cad10358009685a3a52ae9d28daf820fef37
musfiqurofficial <musfiqurofficial1999@gmail.com>
2022-11-30 01:56:30 +0600

    error page create

--- src/error/ErrorPage.jsx
+++ src/error/ErrorPage.jsx
@@ -4,7 +4,7 @@ const ErrorPage = () => {
     return (
         <div className='w-5/12 mx-auto my-44 text-gray-600 text-center'>
             <h1 style={{ fontSize: '165px' }} className='text-9xl font-bold'>404</h1>
-            <h3 className='text-4xl font-bold my-5'>Not Found</h3>
+            <h3 className='text-4xl font-bold my-5'>Page Not Found</h3>
             <p>The resource requested could not be found on this server!</p>
         </div>
     );
